7d0810dee681dbf119bfe582137a9a5bf37e2bea
Eman Sherif <emansherif@Emans-MacBook-Pro-2.local>
2024-09-17 16:24:31 -0700

    delete react

--- react-app/README.md
+++
@@ -1,1 +0,0 @@
-This is my personal website created in React and TypeScript 
--- react-app/src/main.tsx
+++
@@ -1,7 +0,0 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import 'bootstrap/dist/css/bootstrap.css'
-
-createRoot(document.getElementById('root')!).render(
-  <App />
-)
