e5f9f86e1b98f2a934b014afec43ab52407fd1c7
eman-sherif <esherif52@gmail.com>
2025-08-02 20:05:01 -0700

    website update (#2)

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@
 import About from "./components/About";
 import "./App.css";
 import "./styles/Global.css";
-import Experience from "./components/Experience";
 import Intro from "./components/Intro";
 import NavBar from "./components/NavBar";
 import Projects from "./components/Projects";
@@ -14,9 +13,9 @@ function App() {
       <div id="content">
         <Intro></Intro>
         <About></About>
-        <Experience></Experience>
-        <NavBar></NavBar>
         <Projects></Projects>
+        <NavBar></NavBar>
+
       </div>
     </div>
   );
